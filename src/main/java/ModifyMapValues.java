0b7b1f891491a4e09b56a8e2f92a16d45a893722
Meghana_N <meghanangwd@gmail.com>
2019-01-11 10:45:29 +0530

    Added source code for exercise-2

--- src/main/java/ModifyMapValues.java
+++ src/main/java/ModifyMapValues.java
@@ -10,9 +10,6 @@ public class ModifyMapValues {
         String key1 = inputMap.get(inputMap.values().toArray()[0]);
         String value1 = inputMap.get(inputMap.keySet().toArray()[0]);
 
-        String key2 = inputMap.get(inputMap.values().toArray()[0]);
-        String value2 = inputMap.get(inputMap.keySet().toArray()[0]);
-
         if(value1 != null)
         {
             inputMap.put("val1"," ");
