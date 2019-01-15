71a5316280f1f54c4ae8d8afa2c15956a3b2906a
Meghana_N <meghanangwd@gmail.com>
2019-01-15 09:35:07 +0530

    refactored exercise 4 student program

--- src/main/java/com/stackroute/pe5/Student.java
+++ src/main/java/com/stackroute/pe5/Student.java
@@ -1,42 +1,49 @@
-//package main.java.com.stackroute.pe5;
-//
-//public class Student {
-//    private int id;
-//    private String name;
-//    private int age;
-//
-//    public Student(int id, String name, int age) {
-//        this.id = id;
-//        this.name = name;
-//        this.age = age;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format("main.java.com.stackroute.pe5.Student{id=%d, name=%s, age=%d}", id, name, age);
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(int age) {
-//        this.age = age;
-//    }
-//}
+package com.stackroute.pe5;
+
+/*Create a Student class that represents the following information of a student: id, name, and age all
+the member variables should be private .
+a. Implement `getter and setter` .
+b. Create a `StudentSorter` class that implements `Comparator interface` .
+c. Write a class `Maintest` create Student class object(minimum 5)
+d. Add these student object into a List of type Student .
+e. Sort the list based on their age in decreasing order, for student having same
+age, sort based on their name.
+f. For students having same name and age, sort them according to their ID.*/
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    //constructor for student class
+    public Student(int id, String name, int age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+
+    //getters and setters for student class
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/stackroute/pe5/StudentSorter.java
+++ src/main/java/com/stackroute/pe5/StudentSorter.java
@@ -1,27 +1,26 @@
-//package main.java.com.stackroute.pe5;
-//
-//import java.util.Comparator;
-//
-//public class StudentSorter implements Comparator<Student> {
-//
-//public int compare( Student student1,Student student2)
-//{
-//
-//    int ageCompare = student1.getAge()-student2.getAge();
-//    int nameCompare = student1.getName().compareTo(student2.getName());
-//
-//    int idCompare=student1.getId()-student2.getId();
-//
-//    if( ageCompare != 0 )
-//    {
-//        return ageCompare;
-//    }
-//    else if( nameCompare != 0 )
-//    {
-//        return nameCompare;
-//    }
-//    else
-//        return idCompare;
-//}
-//}
-//
+package com.stackroute.pe5;
+
+import com.stackroute.pe5.Student;
+import java.util.Comparator;
+
+public class StudentSorter implements Comparator {
+
+    public int compare(Object o1,Object o2) {
+
+        Student student1=(Student)o1;
+        Student student2=(Student)o2;
+
+        //comparing on the basis of age
+        if(student1.getAge()==student2.getAge())
+        {
+            if(student1.getId()>student2.getId())
+                return 1;
+            else
+                return -1;
+        }
+        else if(student1.getAge()<student2.getAge())
+            return 1;
+        else
+            return -1;
+    }
+}
